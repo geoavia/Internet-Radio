5319a01cd35239082f06f7f34d6d531d8e9a88ba
George Gochaleishvili <ggochaleishvili@qarva.com>
2023-09-13 21:45:57 +0400

    remove wakeup debug info

--- src/main.cpp
+++ src/main.cpp
@@ -16,35 +16,10 @@
 
 RTC_DATA_ATTR int bootCount = 0;
 
-void print_wakeup_reason() 
-{
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason)
-  {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
-  }
-}
-
-void print_GPIO_wake_up()
-{
-  uint64_t GPIO_reason = esp_sleep_get_ext1_wakeup_status();
-  Serial.printf("GPIO that triggered the wake up: GPIO %lX\n", GPIO_reason);
-}
-
 void setup()
 {
 	Serial.begin(115200);
 
-	print_wakeup_reason();
-	print_GPIO_wake_up();
 	esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
 
 	// This can be set in the IDE no need for ext library
